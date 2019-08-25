9fc5d5b42c6d157f25d874071654ab6d05529d55
Lewis Collins <lew.collins94@gmail.com>
2019-08-25 15:43:04 +0100

    followed tutorials to setup and connect to mongodb

---
+++ mongodb.py
@@ -0,0 +1,8 @@
+import pymongo
+
+_author_ = "lewis"
+
+uri = "mongodb://Lewis:starwars10@192.168.0.119/dvwa"
+client = pymongo.MongoClient(uri)
+database = client['dvwa']
+collection = database['users']
